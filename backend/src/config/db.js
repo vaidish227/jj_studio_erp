8ee352b4c88c1571c6a803d5c288933439f2b7eb
Vaidish Sawar <vaidishsavar221@gmail.com>
2026-04-30 11:33:49 +0530

    .env setup

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
 
 const connectDb = async () => {
     try {
